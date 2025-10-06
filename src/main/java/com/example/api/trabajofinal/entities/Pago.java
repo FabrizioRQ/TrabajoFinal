8b6d01330e575f948006e468856a03dc463e9964
Cristian Martin Arias Saldaña <142331430+Lacanchaa@users.noreply.github.com>
2025-10-05 22:36:07 -0500

    Pago.java

--- src/main/java/com/example/api/trabajofinal/entities/Pago.java
+++ src/main/java/com/example/api/trabajofinal/entities/Pago.java
@@ -21,20 +21,20 @@ public class Pago {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_pago", nullable = false)
     private Long id;
-
+    // Monto del pago realizado (en la moneda definida por el sistema)
     @NotNull
     @Column(name = "monto", nullable = false)
     private Integer monto;
-
+    // Fecha y hora exacta en que se efectuó el pago
     @NotNull
     @Column(name = "fecha_pago", nullable = false)
     private Instant fechaPago;
-
+    // Método de pago utilizado (por ejemplo: “Tarjeta”, “Efectivo”, “Transferencia”)
     @Size(max = 20)
     @NotNull
     @Column(name = "metodo_pago", nullable = false, length = 20)
     private String metodoPago;
-
+    // Estado actual del pago (por ejemplo: “Completado”, “Pendiente”, “Fallido”)
     @Size(max = 20)
     @NotNull
     @Column(name = "estado", nullable = false, length = 20)
