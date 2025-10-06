bc19f0360767d570e9f22b25b6675e3d92021345
Cristian Martin Arias Saldaña <142331430+Lacanchaa@users.noreply.github.com>
2025-10-05 22:07:57 -0500

    Avatar.java

--- src/main/java/com/example/api/trabajofinal/entities/Avatar.java
+++ src/main/java/com/example/api/trabajofinal/entities/Avatar.java
@@ -11,16 +11,18 @@ import lombok.Setter;
 @Entity
 @Table(name = "avatar")
 public class Avatar {
+    // Identificador único del avatar
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_avatar", nullable = false)
     private Long id;
-
+    // Nombre o título del avatar
     @Size(max = 100)
     @NotNull
     @Column(name = "nombre_avatar", nullable = false, length = 100)
     private String nombreAvatar;
-
+    // Descripción o configuración visual del avatar
+    //Puede almacenar datos en formato texto o JSON que describen su apariencia
     @NotNull
     @Column(name = "apariencia", nullable = false, length = Integer.MAX_VALUE)
     private String apariencia;
